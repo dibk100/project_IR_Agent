08b6db16d15093db169b5d7901570ae04650d84f
di_server <dibk_100@naver.com>
2025-09-30 23:29:45 +0000

    1차 완성

--- automl-agent/run_vllm_server.sh
+++ automl-agent/run_vllm_server.sh
@@ -10,10 +10,9 @@ export HF_TOKEN=$(grep HF_TOKEN .env | cut -d '=' -f2)
 
 # vLLM 서버 실행 
 python -m vllm.entrypoints.openai.api_server \
-    --model /home/dibaeck/hf_cache/mistralai/Mistral-7B-Instruct-v0.3 \
+    --model /home/dibaeck/hf_cache/hub/models--mistralai--Mistral-7B-Instruct-v0.3/snapshots/0d4b76e1efeb5eb6f6b5e757c79870472e04bd3a/ \
     --tensor-parallel-size 1 \
     --dtype bfloat16 \
     --max-model-len 2048 \
-    --gpu-memory-utilization 0.6 \
-    --gpu-memory-utilization 0.6 \
+    --gpu-memory-utilization 0.7 \
     --port 8000
